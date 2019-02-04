55048d865e528a0591b703c8edab8949d281a750
christinastep <christina.stephan@hetic.net>
2019-02-04 23:19:15 +0100

    animation

--- dist/script.d573be0b.js
+++ dist/script.d573be0b.js
@@ -573,7 +573,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61564" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57407" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
